1a8133741dd9f695a6a540aa42db95ce7a4c5544
martin-hamann <martinhamannnielsen@gmail.com>
2022-12-07 15:27:40 +0100

    main update
    tilføjet effekter på navbar og back to top knap

--- javascript.js
+++ javascript.js
@@ -3,4 +3,38 @@ const navLinks = document.querySelector(".nav-links");
  
  menuHamburger.addEventListener('click',()=>{
         navLinks.classList.toggle('mobile-menu')
-        })
+        });
+
+// change background of navbar
+
+function scrollValue() {
+       var navbar = document.getElementById('navbar');
+       var scroll = window.scrollY;
+       if (scroll < 300) {
+           navbar.classList.remove('scrolled');
+       } else {
+           navbar.classList.add('scrolled');
+       }
+   };
+   
+   window.addEventListener('scroll', scrollValue);
+   
+   // get to top button
+   let mybutton = document.getElementById("myBtn");
+   
+   window.onscroll = function() {scrollFunction()};
+   
+   function scrollFunction() {
+     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+       mybutton.style.display = "block";
+     } else {
+       mybutton.style.display = "none";
+     }
+   }
+   
+   // get to top function
+   function topFunction() {
+     document.body.scrollTop = 0;
+     document.documentElement.scrollTop = 0; 
+   }
+   
