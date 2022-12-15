09485cd87aabc6d6669ffbe3e77f235bc721b8ce
martin-hamann <martinhamannnielsen@gmail.com>
2022-12-15 20:09:29 +0100

    miniupdate

--- forside/javascript.js
+++ forside/javascript.js
@@ -1,26 +1,19 @@
-const menuHamburger = document.querySelector(".menu-hamburger");
-const navLinks = document.querySelector(".nav-links");
- 
- menuHamburger.addEventListener('click',()=>{
-        navLinks.classList.toggle('mobile-menu')
-        });
-   
-   // get to top button
-   let mybutton = document.getElementById("myBtn");
-   
-   window.onscroll = function() {scrollFunction()};
-   
-   function scrollFunction() {
-     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-       mybutton.style.display = "block";
-     } else {
-       mybutton.style.display = "none";
-     }
-   }
-   
-   // get to top function
-   function topFunction() {
-     document.body.scrollTop = 0;
-     document.documentElement.scrollTop = 0; 
-   }
-   
+
+// get to top button
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// get to top function
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
