a2251f1529ae0c04d7b398dfa8a97bc2dd6373a6
martin-hamann <martinhamannnielsen@gmail.com>
2022-12-16 04:37:55 +0100

    website umiddelbart færdig

--- AboutBBQ/javascript.js
+++ AboutBBQ/javascript.js
@@ -1,29 +1,19 @@
-const menuHamburger = document.querySelector(".menu-hamburger");
-const navLinks = document.querySelector(".nav-links");
- 
- menuHamburger.addEventListener('click',()=>{
-        navLinks.classList.toggle('mobile-menu')
-        });
 
-// change background of navbar
+// get to top button
+let mybutton = document.getElementById("myBtn");
 
+window.onscroll = function () { scrollFunction() };
 
-   
-   // get to top button
-   let mybutton = document.getElementById("myBtn");
-   
-   window.onscroll = function() {scrollFunction()};
-   
-   function scrollFunction() {
-     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-       mybutton.style.display = "block";
-     } else {
-       mybutton.style.display = "none";
-     }
-   }
-   
-   // get to top function
-   function topFunction() {
-     document.body.scrollTop = 0;
-     document.documentElement.scrollTop = 0; 
-   }
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// get to top function
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
